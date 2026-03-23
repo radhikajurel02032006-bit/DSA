3f4a9556e21129a87f6244f50d038b8b32721079
Radhika <radhikajurel02032006@gmil.com>
2026-03-23 21:48:50 +0530

    sum ques

---
+++ sum.java
@@ -0,0 +1,14 @@
+    import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = n * (n + 1) / 2;
+
+        System.out.println("Sum = " + sum);
+    }
+}
+    
+
